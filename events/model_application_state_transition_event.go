d421f9355e67297e5cac0a0e09c262e26726902f
Oleg <oleg@Olegs-MacBook-Pro.local>
2022-01-17 18:26:42 +0300

    fixed missing application state transition builder

--- events/model_application_state_transition_event.go
+++ events/model_application_state_transition_event.go
@@ -3,5 +3,20 @@ package events
 // ApplicationStateTransitionEvent struct
 type ApplicationStateTransitionEvent struct {
 	Data      ApplicationStateTransitionEventData `json:"data,omitempty"`
-	EventType string                              `json:"event_type,omitempty"`
+	EventType EventType                           `json:"event_type,omitempty"`
+}
+
+func NewApplicationStateTransitionEvent() ApplicationStateTransitionEvent {
+	return ApplicationStateTransitionEvent{
+		Data:      ApplicationStateTransitionEventData{},
+		EventType: ApplicationStateTransitionEventType,
+	}
+}
+
+func (e ApplicationStateTransitionEvent) data() interface{} {
+	return e.Data
+}
+
+func (e ApplicationStateTransitionEvent) eventType() EventType {
+	return e.EventType
 }
